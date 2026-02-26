be9f0d99102bc0140797285eb1401f204d5c984c
zloot <152008955+zloot63@users.noreply.github.com>
2026-02-27 08:08:00 +0900

    update

--- app/[locale]/[...not found]/page.tsx
+++ app/[locale]/[...not found]/page.tsx
@@ -1,8 +1,6 @@
 import { notFound } from 'next/navigation'
 import type { ReactElement } from 'react'
 
-export const runtime = 'edge'
-
 export default function NotFoundCatchAll(): ReactElement {
   notFound()
 }
