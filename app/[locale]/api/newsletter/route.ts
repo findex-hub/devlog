070b2aac862db1a74ba4eaf5086f96ac970d93ac
zloot <152008955+zloot63@users.noreply.github.com>
2026-02-27 08:19:14 +0900

    update

--- app/[locale]/api/newsletter/route.ts
+++ app/[locale]/api/newsletter/route.ts
@@ -1,6 +1,8 @@
 import siteMetadata from '@/data/siteMetadata'
 import { NewsletterAPI } from 'pliny/newsletter'
 
+export const runtime = 'edge'
+
 const handler = NewsletterAPI({
   provider: siteMetadata.newsletter?.provider || 'buttondown',
 })
